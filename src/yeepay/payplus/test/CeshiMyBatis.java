7ef3913fa77c077fa1c2a59e7a5e3b09587eda20
Tian Xuan <16120128@bjtu.edu.cn>
2018-02-27 20:03:21 +0800

    新增 增删改查功能测试

--- src/yeepay/payplus/test/CeshiMyBatis.java
+++ src/yeepay/payplus/test/CeshiMyBatis.java
@@ -29,10 +29,61 @@ public class CeshiMyBatis {
         SqlSession sqlSession = sqlSessionFactory.openSession();    // 获取到 SqlSession
 
         // 调用 mapper 中的方法：命名空间 + id
-        List<Person> personList = sqlSession.selectList("yeepay.payplus/mapper.UserMapper.findAll");
+        List<Person> personList = sqlSession.selectList("yeepay.payplus.mapper.UserMapper.findAll");
 
         for (Person p : personList){
             System.out.println(p);
         }
     }
+
+
+    @Test
+    public void testInsert(){
+        String resource = "sqlMapConfig.xml";           //定位核心配置文件
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);        // 创建 SqlSessionFactory
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();            //获取到 SqlSession
+
+        Person p = new Person();
+        p.setId(2);
+        p.setName("gavin");
+        p.setAge(12);
+
+        sqlSession.insert("yeepay.payplus.mapper.UserMapper.insert", p);
+        sqlSession.commit();            //默认是不自动提交，必须手工提交
+    }
+
+
+    @Test
+    public void testDeleteById(){
+        String resource = "sqlMapConfig.xml";           //定位核心配置文件
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);        // 创建 SqlSessionFactory
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();            // 获取到 SqlSession
+
+        sqlSession.delete("yeepay.payplus.mapper.UserMapper.deleteById", 1);
+        sqlSession.commit();            //默认是不自动提交，必须手工提交
+    }
+
+
+    @Test
+    public void testUpdate() throws IOException {
+        String resource = "sqlMapConfig.xml";           //定位核心配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);        // 创建 SqlSessionFactory
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();            // 获取到 SqlSession
+
+        Person p = sqlSession.selectOne("yeepay.payplus.mapper.UserMapper.get", 2);   // 获得 id=2 的记录
+        p.setName("jane");
+        p.setAge(16);
+
+        sqlSession.insert("yeepay.payplus.mapper.UserMapper.update", p);
+        sqlSession.commit();            //默认是不自动提交，必须手工提交
+    }
+
+
+
 }
